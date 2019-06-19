a9f62b7ccb91f6cb609b4fd091d8d69632c031f0
Charles D <charlesgithub@gmail.com>
2019-06-19 18:06:04 -0400

    Minor Updates

--- LL.java
+++ LL.java
@@ -232,6 +232,22 @@ public class LL {
 	}
 	
 	/**
+	 * Prints out the Available ASCII Palette
+	 */
+	public static void DrawPalette() {
+		// ROWS = ACROSS
+		// COLUMS DOWN
+		// Foreground (Text Colors)
+		for (int i = 0; i < 256; i++) {
+			System.out.print(((char) 27) + "[38;5;" + i + "m" + i + LL.RESET);
+		}
+		// Backgrounds (Background Colors)
+		for (int i = 0; i < 256; i++) {
+			System.out.print(((char) 27) + "[48;5;" + i + "m" + i + LL.RESET);
+		}
+	}
+	
+	/**
 	 * True/False Method for OS CHECK.
 	 * 
 	 * @return Returns TRUE if Windows OS
@@ -288,6 +304,26 @@ public class LL {
 			return;
 		}
 		
+	}
+	
+	/**
+	 * Sets the Console Window Title
+	 * 
+	 * 
+	 */
+	public static void setConsoleWindowTitle(String newTitle) {
+		if (isWindows()) {
+			try {
+				new ProcessBuilder("cmd", "/c", "title " + newTitle).inheritIO().start().waitFor();
+			}
+			catch (InterruptedException e) {
+				// e.printStackTrace();
+			}
+			catch (IOException e) {
+				// e.printStackTrace();
+			}
+			return;
+		}
 	}
 	
 	/**
@@ -1223,26 +1259,6 @@ public class LL {
 	public static final String	CYAN	= ((char) 27) + "[36m";	// CYAN
 	public static final String	WHITE	= ((char) 27) + "[37m";	// WHITE
 	
-	// Bold
-	public static final String	BLACK_BOLD	= ((char) 27) + "[1;30m";	// BLACK
-	public static final String	RED_BOLD	= ((char) 27) + "[1;31m";	// RED
-	public static final String	GREEN_BOLD	= ((char) 27) + "[1;32m";	// GREEN
-	public static final String	YELLOW_BOLD	= ((char) 27) + "[1;33m";	// YELLOW
-	public static final String	BLUE_BOLD	= ((char) 27) + "[1;34m";	// BLUE
-	public static final String	PURPLE_BOLD	= ((char) 27) + "[1;35m";	// PURPLE
-	public static final String	CYAN_BOLD	= ((char) 27) + "[1;36m";	// CYAN
-	public static final String	WHITE_BOLD	= ((char) 27) + "[1;37m";	// WHITE
-	
-	// Underline
-	public static final String	BLACK_UNDERLINED	= ((char) 27) + "[4;30m";	// BLACK
-	public static final String	RED_UNDERLINED		= ((char) 27) + "[4;31m";	// RED
-	public static final String	GREEN_UNDERLINED	= ((char) 27) + "[4;32m";	// GREEN
-	public static final String	YELLOW_UNDERLINED	= ((char) 27) + "[4;33m";	// YELLOW
-	public static final String	BLUE_UNDERLINED		= ((char) 27) + "[4;34m";	// BLUE
-	public static final String	PURPLE_UNDERLINED	= ((char) 27) + "[4;35m";	// PURPLE
-	public static final String	CYAN_UNDERLINED		= ((char) 27) + "[4;36m";	// CYAN
-	public static final String	WHITE_UNDERLINED	= ((char) 27) + "[4;37m";	// WHITE
-	
 	// Background
 	public static final String	BLACK_BACKGROUND	= ((char) 27) + "[40m";	// BLACK
 	public static final String	RED_BACKGROUND		= ((char) 27) + "[41m";	// RED
@@ -1253,35 +1269,11 @@ public class LL {
 	public static final String	CYAN_BACKGROUND		= ((char) 27) + "[46m";	// CYAN
 	public static final String	WHITE_BACKGROUND	= ((char) 27) + "[47m";	// WHITE
 	
-	// High Intensity
-	public static final String	BLACK_BRIGHT	= ((char) 27) + "[0;90m";	// BLACK
-	public static final String	RED_BRIGHT		= ((char) 27) + "[0;91m";	// RED
-	public static final String	GREEN_BRIGHT	= ((char) 27) + "[0;92m";	// GREEN
-	public static final String	YELLOW_BRIGHT	= ((char) 27) + "[0;93m";	// YELLOW
-	public static final String	BLUE_BRIGHT		= ((char) 27) + "[0;94m";	// BLUE
-	public static final String	PURPLE_BRIGHT	= ((char) 27) + "[0;95m";	// PURPLE
-	public static final String	CYAN_BRIGHT		= ((char) 27) + "[0;96m";	// CYAN
-	public static final String	WHITE_BRIGHT	= ((char) 27) + "[0;97m";	// WHITE
-	
-	// Bold High Intensity
-	public static final String	BLACK_BOLD_BRIGHT	= ((char) 27) + "[1;90m";	// BLACK
-	public static final String	RED_BOLD_BRIGHT		= ((char) 27) + "[1;91m";	// RED
-	public static final String	GREEN_BOLD_BRIGHT	= ((char) 27) + "[1;92m";	// GREEN
-	public static final String	YELLOW_BOLD_BRIGHT	= ((char) 27) + "[1;93m";	// YELLOW
-	public static final String	BLUE_BOLD_BRIGHT	= ((char) 27) + "[1;94m";	// BLUE
-	public static final String	PURPLE_BOLD_BRIGHT	= ((char) 27) + "[1;95m";	// PURPLE
-	public static final String	CYAN_BOLD_BRIGHT	= ((char) 27) + "[1;96m";	// CYAN
-	public static final String	WHITE_BOLD_BRIGHT	= ((char) 27) + "[1;97m";	// WHITE
-	
-	// High Intensity backgrounds
-	public static final String	BLACK_BACKGROUND_BRIGHT		= ((char) 27) + "[0;100m";	// BLACK
-	public static final String	RED_BACKGROUND_BRIGHT		= ((char) 27) + "[0;101m";	// RED
-	public static final String	GREEN_BACKGROUND_BRIGHT		= ((char) 27) + "[0;102m";	// GREEN
-	public static final String	YELLOW_BACKGROUND_BRIGHT	= ((char) 27) + "[0;103m";	// YELLOW
-	public static final String	BLUE_BACKGROUND_BRIGHT		= ((char) 27) + "[0;104m";	// BLUE
-	public static final String	PURPLE_BACKGROUND_BRIGHT	= ((char) 27) + "[0;105m";	// PURPLE
-	public static final String	CYAN_BACKGROUND_BRIGHT		= ((char) 27) + "[0;106m";	// CYAN
-	public static final String	WHITE_BACKGROUND_BRIGHT		= ((char) 27) + "[0;107m";	// WHITE
+	// Special Additions
+	public static final String	UNDERLINE			= ((char) 27) + "[4m";
+	public static final String	CANCEL_UNDERLINE	= ((char) 27) + "[24m";
+	public static final String	REVERSED			= ((char) 27) + "[7m";
+	public static final String	BOLD				= ((char) 27) + "[1m";
 	
 	// ================================
 	
